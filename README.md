68926ba4267fad82fa5f5233f65d6e3fecf17214
Park.Lu <park.lu@nogle.com>
2021-05-04 17:44:04 +0800

    add prettier

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FTX-look Order Book
 
-BTSE Spot websocket APIs
+[BTSE Spot websocket messages](https://www.btse.com/apiexplorer/spot/#websocketmessages)
 
 [Question](https://www.notion.so/Order-Book-d3af33a5722440939ec28b53d943144e)
--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useEffect } from 'react'
+
+const BTSE_SPOT_WEBSOCKET_URL = 'wss://ws.btse.com/ws/spot'
+// const FTX_WEBSOCKET_URL = 'wss://ftx.com/ws/'
+
 function App() {
-  return (
-    <div>
-        FTX-look order book (source: BTSE spot websocket data)
-    </div>
-  );
+  useEffect(() => {
+    const ws = new WebSocket(BTSE_SPOT_WEBSOCKET_URL)
+
+    ws.onopen = () => {
+      console.log('websocket is open now')
+      ws.send(
+        JSON.stringify({
+          op: 'subscribe',
+          args: ['orderBookApi:BTSE-USDT_0'],
+        }),
+      )
+    }
+    ws.onmessage = (evt) => {
+      // console.log(evt.data)
+    }
+    ws.onclose = () => {
+      console.log('websocket is closed')
+    }
+    ws.onerror = (error) => {
+      console.log(error)
+    }
+  })
+
+  return <div>FTX-look order book (source: BTSE spot websocket data)</div>
 }
 
-export default App;
+export default App
