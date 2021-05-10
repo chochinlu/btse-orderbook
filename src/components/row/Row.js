67250e1d07e4456875548eeccb74c63e2cb04850
Park.Lu <park.lu@nogle.com>
2021-05-10 11:43:50 +0800

    fix flash property

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,21 +1,43 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const buyFlash = keyframes`
+  0%, 100% {
+    background-color: transparent;
+  }
+  50% {
+    background-color: rgba(2, 199, 122, 0.25);
+  }
+`
 
 export const BuyRow = styled.tr`
   border-bottom: 1px solid rgba(0, 0, 0, 0.075);
-  background: linear-gradient(
+  background-image: linear-gradient(
     to left,
     rgba(2, 199, 122, 0.25),
     rgba(2, 199, 122, 0.25) ${(props) => props.percent}%,
     rgba(0, 0, 0, 0) ${(props) => props.percent}%
   );
+  animation-name: ${props => props.isChanged ? buyFlash : 'none'};
+  animation-duration: 0.3s;
+`
+
+const sellFlash = keyframes`
+  0%, 100% {
+    background-color: transparent;
+  }
+  50% {
+    background-color: rgba(255, 59, 105, 0.25);
+  }
 `
 
 export const SellRow = styled.tr`
   border-bottom: 1px solid rgba(0, 0, 0, 0.075);
-  background: linear-gradient(
+  background-image: linear-gradient(
     to right,
     rgba(255, 59, 105, 0.25),
     rgba(255, 59, 105, 0.25) ${(props) => props.percent}%,
     rgba(0, 0, 0, 0) ${(props) => props.percent}%
   );
+  animation-name: ${props => props.isChanged ? sellFlash : 'none'};
+  animation-duration: 0.3s;
 `
