509cbb3fb93775bef63ba07d25848946b1ffa1f5
Park.Lu <park.lu@nogle.com>
2021-05-11 15:18:30 +0800

    use basic react style

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,43 +1,31 @@
-import styled, { keyframes } from 'styled-components'
+import './Row.css'
 
-const buyFlash = keyframes`
-  0%, 100% {
-    background-color: transparent;
-  }
-  50% {
-    background-color: rgba(2, 199, 122, 0.25);
-  }
-`
+export function BuyRow(props) {
+    const style = {
+        backgroundImage: `linear-gradient(
+            to left,
+            rgba(2, 199, 122, 0.25),
+            rgba(2, 199, 122, 0.25) ${props.percent}%,
+            rgba(0, 0, 0, 0) ${props.percent}%
+        `,
+        animationName: props.isChanged ? 'buyFlash' : 'none',
+        animationDuration: '0.2s'
+    }
 
-export const BuyRow = styled.tr`
-  border-bottom: 1px solid rgba(0, 0, 0, 0.075);
-  background-image: linear-gradient(
-    to left,
-    rgba(2, 199, 122, 0.25),
-    rgba(2, 199, 122, 0.25) ${(props) => props.percent}%,
-    rgba(0, 0, 0, 0) ${(props) => props.percent}%
-  );
-  animation-name: ${props => props.isChanged ? buyFlash : 'none'};
-  animation-duration: 0.3s;
-`
+    return <tr className={'row'} style={style}>{props.children}</tr>
+}
 
-const sellFlash = keyframes`
-  0%, 100% {
-    background-color: transparent;
-  }
-  50% {
-    background-color: rgba(255, 59, 105, 0.25);
-  }
-`
+export function SellRow(props) {
+    const style = {
+        backgroundImage: `linear-gradient(
+            to right,
+            rgba(255, 59, 105, 0.25),
+            rgba(255, 59, 105, 0.25) ${props.percent}%,
+            rgba(0, 0, 0, 0) ${props.percent}%
+        `,
+        animationName: props.isChanged ? 'sellFlash' : 'none',
+        animationDuration: '0.2s'
+    }
 
-export const SellRow = styled.tr`
-  border-bottom: 1px solid rgba(0, 0, 0, 0.075);
-  background-image: linear-gradient(
-    to right,
-    rgba(255, 59, 105, 0.25),
-    rgba(255, 59, 105, 0.25) ${(props) => props.percent}%,
-    rgba(0, 0, 0, 0) ${(props) => props.percent}%
-  );
-  animation-name: ${props => props.isChanged ? sellFlash : 'none'};
-  animation-duration: 0.3s;
-`
+    return <tr className={'row'}  style={style}>{props.children}</tr>
+}
