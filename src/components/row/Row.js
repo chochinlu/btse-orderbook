98e6bace042e084e0d3c0f9a4f41f56d359db878
Park.Lu <park.lu@nogle.com>
2021-06-08 21:20:52 +0800

    use more classes to replace style

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,6 +1,7 @@
 import './Row.css'
 import styled from 'styled-components'
 import { toLocale } from '../../util/util'
+import classNames from 'classnames'
 
 const Ask = styled.span`
   font-weight: bold;
@@ -15,7 +16,6 @@ const Bid = styled.span`
 export function Row(props) {
   const direction = props.isBid ? 'left' : 'right'
   const color = props.isBid ? 'rgba(2, 199, 122, 0.25)' : 'rgba(255, 59, 105, 0.25)'
-  const flash = props.isBid ? 'buyFlash' : 'sellFlash'
 
   const style = {
     backgroundImage: `linear-gradient(
@@ -24,25 +24,29 @@ export function Row(props) {
             ${color} ${props.percent}%,
             rgba(0, 0, 0, 0) ${props.percent}%
         `,
-    animationName: props.isChanged ? flash : 'none',
-    animationDuration: '0.2s',
-  }
-
-  const tdStyle = {
-    textAlign: props.isBid ? 'right' : 'left',
   }
 
   const handleEnter = () => {
     props.handleMouseEnter(props.quote)
   }
 
+  const rowClasses = () => {
+    return classNames('row', {
+      'buy-animation': props.isChanged && props.isBid,
+      'sell-animation': props.isChanged && !props.isBid,
+    })
+  }
+
+  const cellClasses = () => {
+    return classNames('cell', { tl: !props.isBid, tr: props.isBid })
+  }
 
   return (
-    <tr className={'row'} style={style} onMouseEnter={handleEnter}>
-      <td className={'cell'} style={tdStyle}>
+    <tr className={rowClasses()} style={style} onMouseEnter={handleEnter}>
+      <td className={cellClasses()}>
         {props.isBid ? toLocale(props.quote.size) : <Ask>{toLocale(props.quote.price)}</Ask>}
       </td>
-      <td className={'cell'} style={tdStyle}>
+      <td className={cellClasses()}>
         {props.isBid ? <Bid>{toLocale(props.quote.price)}</Bid> : toLocale(props.quote.size)}
       </td>
     </tr>
